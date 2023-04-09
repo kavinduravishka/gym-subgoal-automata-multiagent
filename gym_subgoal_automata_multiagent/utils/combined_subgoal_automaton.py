87a62b8335d17eeb3a7dcaa867e5a254cde1a178
kavinduravishka <kavinduravishka98sl@gmail.com>
2023-04-09 22:53:15 +0530

    remove unwanted files from tracking

--- gym_subgoal_automata_multiagent/utils/combined_subgoal_automaton.py
+++
@@ -1,7 +0,0 @@
-from gym_subgoal_automata_multiagent.utils.subgoal_automaton import SubgoalAutomaton
-
-class CombinedSubgoalAutomaton(SubgoalAutomaton):
-    def __init__(self):
-        super().__init__()
-
-    
